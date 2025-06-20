16d92ac2333cb83ec09fbaa54b728a9c28ef12cc
eyob-wendmagegn <eyobwende18@gmail.com>
2025-06-20 01:21:07 -0700

    update ones

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import image3 from './image/image3.JPG'; // Verify this path
+//import image3 from './image/image3.JPG'; // Verify this path
 
 const About = () => {
   const [aboutContent, setAboutContent] = useState('');
@@ -22,7 +22,7 @@ const About = () => {
           </div>
           <div className="md:w-1/2 flex justify-center mt-6 md:mt-0">
             <img
-              src={image3} // Use the imported image
+              src="src/assets/image3.jpg" // Use the imported image
               alt="About Me"
               loading="lazy"
               className="w-full max-w-md h-auto rounded-xl shadow-lg object-cover transition-transform duration-300 hover:scale-105"
