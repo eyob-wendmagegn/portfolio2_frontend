6cb6469f59f3a9262f0e4e407cb9fac138c59b40
eyob-wendmagegn <eyobwende18@gmail.com>
2025-06-19 23:49:44 -0700

    comment a

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),],
   build: {
     outDir: 'dist', // Ensure output directory is 'dist'
   },
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.pdf', '**/*.doc', '**/*.docx'],
-  // Optional: Set base path for Render deployment
-  base: process.env.NODE_ENV === 'production' ? '/portfolio1/' : '/',
+  base: '/', // Set to '/' for root deployment on Render
 });
